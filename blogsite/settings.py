66fa268af43aa2e5f952feb92e8d316e679f3421
iamsushil08 <iamsushil01@gmail.com>
2026-08-30 18:51:31 +0545

    Configure Resend email service

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     'products',
     'cloudinary_storage',
     'cloudinary',
+    'anymail',
    
     
 ]
@@ -168,14 +169,10 @@ CLOUDINARY_STORAGE = {
 }
 
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
 
-EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
-EMAIL_USE_TLS = False
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+ANYMAIL = {
+    "RESEND_API_KEY": os.environ.get("RESEND_API_KEY"),
+}
 
-EMAIL_TIMEOUT = 5
+DEFAULT_FROM_EMAIL = "ChessWithBiBu <noreply@bibekpandey01.com.np>"
